8d21843f9df75829ac642e5cb51af1625a082747
Rodrigo Vilar <rvilar@gmail.com>
2019-10-15 11:43:00 -0300

    T001 Create automated tests for BurgosAgency Initial configuration - For review

---
+++ src/main/java/br/ufpb/dcx/esa/medievalbank/MedievalbankApplication.java
@@ -0,0 +1,13 @@
+package br.ufpb.dcx.esa.medievalbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedievalbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedievalbankApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/ufpb/dcx/esa/medievalbank/service/BurgosAgency.java
@@ -0,0 +1,26 @@
+package br.ufpb.dcx.esa.medievalbank.service;
+
+
+public class BurgosAgency {
+
+	public static void setName(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static void setManager(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static String getManager() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
