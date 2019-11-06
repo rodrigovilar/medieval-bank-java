2eaf86c69d91ed0f1e2b9cf39c760f27b11c96eb
john-lima <john.lennon@dcx.ufpb.br>
2019-11-06 10:02:20 -0300

    implementing test 12 - Atendee e-mail format is invalid

--- src/main/java/br/ufpb/dcx/esa/medievalbank/service/AtendeeService.java
+++ src/main/java/br/ufpb/dcx/esa/medievalbank/service/AtendeeService.java
@@ -1,13 +1,16 @@
 package br.ufpb.dcx.esa.medievalbank.service;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 import org.springframework.stereotype.Service;
 
 import br.ufpb.dcx.esa.medievalbank.MedievalBankException;
+import ch.qos.logback.core.boolex.Matcher;
 
 @Service
 public class AtendeeService {
+	 	 
 
 	public Atendee create(Atendee atendee) {
 		return null;
@@ -19,12 +22,20 @@ public class AtendeeService {
 	}
 
 	public Atendee update(Atendee atendee) {
-		
+		final String regex ="^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
+		Pattern pattern = Pattern.compile(regex);
+	    java.util.regex.Matcher matcher = pattern.matcher(atendee.getEmail());
+
 		if(atendee.getName() == null) {
 			throw new MedievalBankException("Name is mandatory");
 		}
+		if(!matcher.matches()) {
+			throw new MedievalBankException("Atendee e-mail format is invalid");
+		}
+		
 		
 		return null;
+
 	}
 
 	public void delete(Atendee atendee) {
