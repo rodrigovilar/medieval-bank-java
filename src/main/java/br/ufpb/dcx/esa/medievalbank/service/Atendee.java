13efd46e5dd4c83cc4edd827e3e9c8ab9496de09
wendelrenato <wendel.renato@dce.ufpb.br>
2019-11-26 11:50:55 -0300

    Refatoramento

--- src/main/java/br/ufpb/dcx/esa/medievalbank/service/Atendee.java
+++ src/main/java/br/ufpb/dcx/esa/medievalbank/service/Atendee.java
@@ -62,4 +62,50 @@ public class Atendee implements Serializable{
 		return this.ssn;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((creation == null) ? 0 : creation.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((ssn == null) ? 0 : ssn.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Atendee other = (Atendee) obj;
+		if (creation == null) {
+			if (other.creation != null)
+				return false;
+		} else if (!creation.equals(other.creation))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (ssn == null) {
+			if (other.ssn != null)
+				return false;
+		} else if (!ssn.equals(other.ssn))
+			return false;
+		return true;
+	}
+
 }
--- src/main/java/br/ufpb/dcx/esa/medievalbank/service/AtendeeService.java
+++ src/main/java/br/ufpb/dcx/esa/medievalbank/service/AtendeeService.java
@@ -29,96 +29,41 @@ public class AtendeeService {
 
 	public Atendee create(Atendee atendee){
 		
-		if(atendee.getName() == null) {
-			throw new MedievalBankException("Name is mandatory"); 
-		}
+		if(atendee.getName() == null) throw new MedievalBankException("Name is mandatory"); 
 
-		if (repository.existsByName(atendee.getName())) {
-			throw new MedievalBankException("Atendee name cannot be duplicated");
-		}
+		validateDuplicatedName(atendee);
 		Integer id = atendee.getId();// a conversão de um int null para integer gera um inteiro
 		
-		if (id != null && id != 0 ) {
-			throw new MedievalBankException("Atendee id cannot be set");
-		}
-		if (atendee.getCreation() != null) {
-			throw new MedievalBankException("Atendee creation date cannot be set");
-		}
-		if(atendee.getEmail() != null) {
-			if(!matchersRegex(atendee.getEmail())) {
-				throw new MedievalBankException("Atendee e-mail format is invalid");
-			}
-		}
+		if (id != null && id != 0 ) throw new MedievalBankException("Atendee id cannot be set");
+		
+		if (atendee.getCreation() != null) throw new MedievalBankException("Atendee creation date cannot be set");
+		validateEmail(atendee);
 		
 		
 		atendee.setCreation(new Date());
 		return repository.save(atendee);
 	}
 
-	public Atendee getOne(Integer id) {
-		
-		try {		
-			Atendee atendee = repository.getOne(id);
-			atendee.getId();
-			return atendee;
 
-		} catch (JpaObjectRetrievalFailureException e) {
-			throw new MedievalBankException("Unknown Atendee id: " + id);
-		} catch (EntityNotFoundException e) {
-			throw new MedievalBankException("Atendee id not found: " + id);			
-		}
-		
-	}
 
 	public Atendee update(Atendee atendee) {
 		
-		/*if(atendee.getName() == null) {
-			if(repository.existsById(atendee.getId()) == false){
-				throw new MedievalBankException("Atendee id not found: "+atendee.getId());
-			}
-			
-			throw new MedievalBankException("Name is mandatory");
-		}
-		else {
-			if(repository.existsById(atendee.getId()) == false ) {
-				throw new MedievalBankException("Atendee id not found: " + atendee.getId());
-			}
-		}*/
-		
-
-		
-		/*if (repository.existsByName(Atendee.getName())) {
-			throw new MedievalBankException("Atendee name cannot be duplicated");
-		}*/
 		Atendee oldAtendee = getOne(atendee.getId());
 		
 		nullSafeEquals(oldAtendee.getSSN(), atendee.getSSN(), "Atendee SSN is immutable");
 		nullSafeEquals(oldAtendee.getCreation(), atendee.getCreation(), "Atendee creation date cannot be changed");
 	
 		
-		if(atendee.getEmail() != null && !matchersRegex(atendee.getEmail())) {
-			throw new MedievalBankException("Atendee e-mail format is invalid");
-		}
-		
-		boolean exist = repository.existsById(atendee.getId());
-		if(exist) {
-			nullSafeEquals(oldAtendee.getId(), atendee.getId(), "Atendee id not found: "+atendee.getId());
-
-		}else{
-			throw new MedievalBankException("Atendee id not found: "+atendee.getId());
-		}
+		validateEmail(atendee);
 		
 		if(atendee.getName() == null) {
 			throw new MedievalBankException("Name is mandatory");
 		
 		} else {
 			if(!(oldAtendee.getName().equals(atendee.getName()))) {
-				if (repository.existsByName(atendee.getName())) {
-					throw new MedievalBankException("Atendee name cannot be duplicated");
-				}
+				validateDuplicatedName(atendee);
 			}
 		}
-		repository.save(atendee);
 		
 		return repository.save(atendee);
 
@@ -142,18 +87,6 @@ public class AtendeeService {
 		}
 	}
 
-	public void delete(Atendee atendee) {
-		if(atendee == null) {
-			throw new MedievalBankException("Null atendee"); 
-		}
-		
-		if(repository.existsById(atendee.getId()) == false){
-			throw new MedievalBankException("Atendee id not found: "+atendee.getId());
-		}
-		
-		repository.delete(atendee);
-		
-	}
 	
 	public List<Atendee> getAll(){
 		return repository.findAll();
@@ -162,4 +95,43 @@ public class AtendeeService {
 		return repository.findByNameContaining(name);
 	}
 
+	private void validateDuplicatedName(Atendee atendee) {
+		if (repository.existsByName(atendee.getName())) {
+			throw new MedievalBankException("Atendee name cannot be duplicated");
+		}
+	}
+	
+	private void validateEmail(Atendee atendee) {
+		if(atendee.getEmail() != null) {
+			if(!matchersRegex(atendee.getEmail())) {
+				throw new MedievalBankException("Atendee e-mail format is invalid");
+			}
+		}
+	}
+	public Atendee getOne(Integer id) {
+		
+		try {		
+			Atendee atendee = repository.getOne(id);
+			atendee.getId();
+			return atendee;
+			
+		} catch (JpaObjectRetrievalFailureException e) {
+			throw new MedievalBankException("Unknown Atendee id: " + id);
+		} catch (EntityNotFoundException e) {
+			throw new MedievalBankException("Atendee id not found: " + id);			
+		}
+		
+	}
+	public void delete(Atendee atendee) {
+		if(atendee == null) {
+			throw new MedievalBankException("Null atendee"); 
+		}
+		
+		if(repository.existsById(atendee.getId())){
+			throw new MedievalBankException("Atendee id not found: "+atendee.getId());
+		}
+		
+		repository.delete(atendee);
+		
+	}
 }
