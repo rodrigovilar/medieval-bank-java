f435f4c3ef445fc36223fd30cb307a411bf4436d
Hercules Silva de Souza <hercules.silva@dcx.ufpb.br>
2020-02-04 11:32:52 -0300

    t034 - fix import optional

--- src/main/java/br/ufpb/dcx/esa/medievalbank/service/DemandService.java
+++ src/main/java/br/ufpb/dcx/esa/medievalbank/service/DemandService.java
@@ -1,7 +1,6 @@
 package br.ufpb.dcx.esa.medievalbank.service;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
