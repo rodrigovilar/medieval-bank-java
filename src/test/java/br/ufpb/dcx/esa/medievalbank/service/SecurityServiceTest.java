20bba1c46fbc4f6dfc2f3a0795c5b4c019974e9b
Ruan Carlos <ruan.carlos@dce.ufpb.br>
2020-03-20 16:44:33 -0300

    Implemented test exceptionInsertAttendeeWithoutPermission

--- src/test/java/br/ufpb/dcx/esa/medievalbank/service/SecurityServiceTest.java
+++ src/test/java/br/ufpb/dcx/esa/medievalbank/service/SecurityServiceTest.java
@@ -1,7 +1,7 @@
 package br.ufpb.dcx.esa.medievalbank.service;
 
-import static br.ufpb.dcx.esa.medievalbank.service.SecurityServiceTestHelper.tryLoginUnsuccessfully;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,21 +12,15 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import br.ufpb.dcx.esa.medievalbank.MedievalBankException;
 import br.ufpb.dcx.esa.medievalbank.command.Command;
 import br.ufpb.dcx.esa.medievalbank.command.InsertAttendee;
 import br.ufpb.dcx.esa.medievalbank.model.Atendee;
-import br.ufpb.dcx.esa.medievalbank.model.User;
 
 @RunWith(SpringRunner.class)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest
 public class SecurityServiceTest {
-	
-	@Autowired
-	private SecurityService service;
-	
-	@Autowired
-	private UserService userService;
 	
 	@Autowired
     private AgencyService agencyService;
@@ -38,24 +32,15 @@ public class SecurityServiceTest {
         return atendee;
     }
 	
-	@Test
-	@Transactional
-	public void loginSuccessfully() {
-		User user = new User("ruaanc", "12345");
-		userService.create(user);
-		service.login("ruaanc", "12345");
-		assertEquals(user.isLogged(), true);
-	}
-	
-	@Test
-	@Transactional
-	public void loginUnsuccessfully() {
-		User user = new User("francisco", "12345");
-		userService.create(user);
-		String failMessage = "Test failed because the user was able to log in with the incorrect credentials.";
-		String expectedExceptionMessage = "Username or Password is incorrect";
-		tryLoginUnsuccessfully(service, "francisco", "123456", failMessage, expectedExceptionMessage);
-		tryLoginUnsuccessfully(service, "francis", "12345", failMessage, expectedExceptionMessage);
+	public void tryinsertAttendeeWithError(AgencyService service, Atendee atendee, String failMessage,
+			String expectedExceptionMessage) {
+		try {
+			Command insertAttendee = new InsertAttendee(atendee);
+			service.execute(insertAttendee);
+			fail(failMessage);
+		} catch (MedievalBankException e) {
+			assertEquals(expectedExceptionMessage, e.getMessage());
+		}
 	}
 	
 	@Test
@@ -68,7 +53,15 @@ public class SecurityServiceTest {
 		assertEquals(atendee, response);
 	}
 	
-	
+	@Test
+	@WithMockUser(username = "john", roles = { "SYSTEM" })
+	@Transactional
+	public void exceptionInsertAttendeeWithoutPermission() {
+		Atendee atendee = builAttendee("A1", "a@gmail.com");
+		String failMessage = "Test failed because the system accepted insert attendee without permission";
+		String expectedExceptionMessage = "User is not allowed";
+		tryinsertAttendeeWithError(agencyService, atendee, failMessage, expectedExceptionMessage);
+	}
 	
 
 }
